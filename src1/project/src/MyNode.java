ff5f8d6730c53d7e9ade39ac2bfb317b9eb1798a
Robin Wiberg <dat14rwi@student.lu.se>
2018-12-07 11:08:28 +0100

    experiment

--- src1/project/src/MyNode.java
+++ src1/project/src/MyNode.java
@@ -2,6 +2,7 @@ package tree;
 //Merge this biatch!
 //Changes
 //Look at my changes!
+//Volatile experiment...might explode
 
 /**
  * This is a node in a binary search tree. The node holds an integer value.
